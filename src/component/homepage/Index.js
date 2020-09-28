639c2a5e672a6df9bf11fe2645b361998e480582
ariefnugraha <muhammadariefnugraha77@gmail.com>
2020-09-28 13:23:46 +0700

    Install react bootstrap

---
+++ src/component/homepage/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Welcome from './Welcome';
+const Index = () => {
+    return (
+        <Welcome />
+    )
+}
+
+export default Index;
---
+++ src/component/homepage/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './css/welcome.module.css';
+
+const Welcome = () => {
+    return (
+        <h1 className={style.title}>Halo</h1>
+    )
+}
+
+export default Welcome;
